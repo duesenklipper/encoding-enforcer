81553146bec49ee44717935292084f5683dd2d5c
Carl-Eric Menzel <cmenzel@wicketbuch.de>
2015-06-20 13:13:55 +0200

    work in progress

--- src/main/java/de/duesenklipper/maven/enforcer/EncodingEnforcer.java
+++ src/main/java/de/duesenklipper/maven/enforcer/EncodingEnforcer.java
@@ -4,11 +4,20 @@ import org.apache.maven.enforcer.rule.api.EnforcerRule;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
 import org.apache.maven.plugin.logging.Log;
+import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
+
+import java.io.File;
 
 public class EncodingEnforcer implements EnforcerRule {
     public void execute(EnforcerRuleHelper helper) throws EnforcerRuleException {
         Log log = helper.getLog();
         log.info("EncodingEnforcer activated");
+        try {
+            String sourceDirPath = (String) helper.evaluate("${project.source.directory}");
+            File sourceDir = new File(sourceDirPath);
+        } catch (ExpressionEvaluationException e) {
+            throw new EnforcerRuleException("error", e);
+        }
     }
 
     public boolean isCacheable() {
